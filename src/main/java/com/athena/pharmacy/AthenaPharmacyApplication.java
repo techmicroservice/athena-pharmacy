81978a260bc135c1e3cdb23b605ed259312a7d40
Chandu <chanduonedrive@gmail.com>
2023-01-26 16:20:04 +0530

    athena pharmasy first commit

---
+++ src/main/java/com/athena/pharmacy/AthenaPharmacyApplication.java
@@ -0,0 +1,13 @@
+package com.athena.pharmacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AthenaPharmacyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AthenaPharmacyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/athena/pharmacy/AthenaPharmacyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.athena.pharmacy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AthenaPharmacyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
